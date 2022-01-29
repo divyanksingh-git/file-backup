19f248f0a49cf2f57ff63b96b40b8f216e682cae
Divyank Singh <divyanksingh20@gmail.com>
2022-01-29 14:22:26 +0000

    Update server.py

--- server.py
+++ server.py
@@ -13,6 +13,9 @@ def recvall(c):
             break
     return data
 
+if not os.path.exists("data"):
+	os.mkdir("data")
+    
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.settimeout(600)
 HOST='127.0.0.1'
